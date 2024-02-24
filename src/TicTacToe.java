ee121e84f70aa291deb5e99e5f38f147df46592b
UTLB216 <UTLB216@LAPTOP-H63F0F0N>
2024-02-24 09:35:41 +0530

    Initial Commit

---
+++ src/TicTacToe.java
@@ -0,0 +1,193 @@
+import java.util.Random;
+import java.util.Scanner;
+
+class TicTacToe {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);
+	char[][] board = {{' ', ' ', ' '}, 
+			          {' ', ' ', ' '}, 
+			          {' ', ' ', ' '}};
+	
+	printBoard(board);
+	
+
+	
+	while(true) {
+    playerMove(board, scanner);
+    if(isGameOver(board)) {
+    	break;
+    }
+    printBoard(board);
+
+    computerTurn(board);
+    if(isGameOver(board)) {
+    	break;
+    }
+    printBoard(board);
+
+	}
+
+}
+
+	private static boolean isGameOver(char[][] board) {
+		
+	if (whoWonGame(board, 'X')) {
+	printBoard(board);
+	System.out.println("Player Won !!");
+	return true;
+	}
+		
+	if (whoWonGame(board, 'O')) {
+		printBoard(board);
+		System.out.println("Computer Won !!");
+		return true;
+		}	
+		
+	
+	for(int i=0; i < board.length; i++) {
+			for(int j=0; i < board.length; j++) {
+				if (board[i][j] == ' ') {
+					return false;
+				}
+			}
+		}
+		printBoard(board);
+		System.out.println("The game has been tied.");
+		return true;
+		
+	}
+
+	private static boolean whoWonGame(char[][] board, char symbol) {
+		if((board[0][0] == symbol && board[0][1] == symbol && board[0][2] == symbol) ||
+		   (board[1][0] == symbol && board[1][1] == symbol && board[1][2] == symbol) ||
+		   (board[2][0] == symbol && board[2][1] == symbol && board[2][2] == symbol) ||
+		   
+		   (board[0][0] == symbol && board[1][0] == symbol && board[2][0] == symbol) ||
+		   (board[0][1] == symbol && board[1][1] == symbol && board[2][1] == symbol) ||
+		   (board[0][2] == symbol && board[1][2] == symbol && board[2][2] == symbol) ||
+		   
+		   (board[0][0] == symbol && board[1][1] == symbol && board[2][2] == symbol) ||
+		   (board[2][0] == symbol && board[1][1] == symbol && board[0][2] == symbol))	{
+			
+			return true;
+		}
+		return false;
+	}
+
+	private static void computerTurn(char[][] board) {
+		Random rand = new Random();
+		int computerMove;
+		while(true) {
+		 computerMove = rand.nextInt(9) + 1;
+		if(isValidMove(board, Integer.toString(computerMove))) {
+			break;
+		  }
+		}
+		System.out.println("Computer Chose" +  computerMove);
+		placeMove(board, Integer.toString(computerMove), 'O');
+	}
+	
+	private static boolean isValidMove (char[][] board, String position) {
+		switch(position) {
+		case "1":
+			return (board[0][0] == ' ');
+			
+		case "2":
+			return (board[0][1] == ' ');
+			
+			
+		case "3":
+			return (board[0][2] == ' ');
+			
+		case "4":
+			return (board[1][0] == ' ');
+			
+		case "5":
+			return (board[1][1] == ' ');
+			
+		case "6":
+			return (board[1][2] == ' ');
+			
+		case "7":
+			return (board[2][0] == ' ');
+			
+		case "8":
+			return (board[2][1] == ' ');
+			
+		case "9":
+			return (board[2][2] == ' ');
+			
+		default:
+				return false;
+		}
+	}
+
+	private static void playerMove(char[][] board, Scanner scanner) {
+		String userInput;
+		while(true) {
+		System.out.println("From what position you would like to start? (1-9)");
+	    userInput = scanner.nextLine();
+		if (isValidMove(board, userInput)) {
+			break;
+		} else {
+			System.out.println(userInput + "is not a valid move.");
+		}
+	}
+		
+		placeMove(board, userInput, 'X');
+		
+	}
+
+	private static void placeMove(char[][] board, String position, char symbol) {
+		switch(position) {
+		case "1":
+			board[0][0] = symbol;
+			break;
+			
+		case "2":
+			board[0][1] = symbol;
+			break;
+			
+		case "3":
+			board[0][2] = symbol;
+			break;
+			
+		case "4":
+			board[1][0] = symbol;
+			break;
+			
+		case "5":
+			board[1][1] = symbol;
+			break;
+			
+		case "6":
+			board[1][2] = symbol;
+			break;
+			
+		case "7":
+			board[2][0] = symbol;
+			break;
+			
+		case "8":
+			board[2][1] = symbol;
+			break;
+			
+		case "9":
+			board[2][2] = symbol;
+			break;
+   default:
+		   System.out.println(":(");
+		}
+	}
+
+	private static void printBoard(char[][] board) {
+		System.out.println(board[0][0] + "|" + board[0][1] + "|" + board[0][2]);
+		System.out.println("-+-+-");
+		System.out.println(board[1][0] + "|" + board[1][1] + "|" + board[1][2]);
+		System.out.println("-+-+-");
+		System.out.println(board[2][0] + "|" + board[2][1] + "|" + board[2][2]);
+	}
+
+}
